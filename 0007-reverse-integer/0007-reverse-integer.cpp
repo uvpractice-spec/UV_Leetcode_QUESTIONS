829c90cb7f5e13398365aa52d196315c06fd5603
uvpractice-spec <uvpracticequestion@gmail.com>
2026-08-07 18:21:43 +0530

    Time: 3 ms (29.36%), Space: 8.6 MB (21.31%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;          
+            
+            // Check for overflow before multiplying ans by 10
+            // INT_MAX is 2147483647
+            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && pop > 7)) {
+                return 0;
+            }
+            // Check for underflow before multiplying ans by 10
+            // INT_MIN is -2147483648
+            if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && pop < -8)) {
+                return 0;
+            }
+            
+            ans = ans * 10 + pop; 
+        }
+        
+        return ans;
+    }
+};

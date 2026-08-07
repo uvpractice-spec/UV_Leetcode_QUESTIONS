20b43b5822ecd0eb81af89c6e5ac2e890977413b
uvpractice-spec <uvpracticequestion@gmail.com>
2026-08-07 17:38:31 +0530

    Time: 600 ms (12.19%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,5 +1,23 @@
 # Write your MySQL query statement below this is sql
+/*
+from pyspark.sql import functions as F
 
+# 1. Select only the necessary columns from the manager side before joining.
+# This cuts down data payload volume during the network shuffle phase.
+emp = employee_df.alias("emp")
+mg = employee_df.select("id", "salary").alias("mg")
+
+# 2. Switch to an inner join (implicit optimization from your filter)
+result_df = (
+    emp.join(
+        mg,
+        F.col("emp.managerId") == F.col("mg.id"),
+        how="inner" # Changing to inner avoids unnecessary tracking of unmatched left rows
+    )
+    .filter(F.col("emp.salary") > F.col("mg.salary"))
+    .select(F.col("emp.name").alias("Employee"))
+)
+*/
 select Emp.name as Employee
 from Employee Emp
 inner join Employee mg

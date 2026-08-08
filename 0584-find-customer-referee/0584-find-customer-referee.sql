a8a13a78ca069ef5d650b3e509d8c144d4b7e3b8
uvpractice-spec <uvpracticequestion@gmail.com>
2026-08-08 20:27:34 +0530

    Time: 592 ms (20.22%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -2,3 +2,10 @@
 select name
 from Customer
 where referee_id is null or referee_id <> 2
+
+
+/*
+
+Customer_df.filter((F.col("referee_id").isNull()) | (F.col("referee_id")!= 2)).select("name")
+
+*/

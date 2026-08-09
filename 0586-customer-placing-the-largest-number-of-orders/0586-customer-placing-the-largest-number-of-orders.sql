1dc5459ff00e8e591b865619ec2698b1d10b2818
uvpractice-spec <uvpracticequestion@gmail.com>
2026-08-09 12:35:39 +0530

    Time: 586 ms (17.72%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -4,3 +4,19 @@ from Orders
 group by customer_number
 order by count(order_number) desc
 limit 1;
+
+
+/*
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+
+# Step 1 Count orders per customer
+counts_df = orders_df.groupBy("customer_number").count()
+
+# Step 2  Use dense_rank to handle ties
+window_spec = Window.orderBy(F.col("count").desc())
+
+result_df = counts_df.withColumn("rank", F.dense_rank().over(window_spec)) \
+    .filter(F.col("rank") == 1) \
+    .select("customer_number")
+*/

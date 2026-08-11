e35250549d88dc21ba27e5f8de8fa55b7d6af0bf
uvpractice-spec <uvpracticequestion@gmail.com>
2026-08-11 10:04:00 +0530

    Time: 0 ms (100%), Space: 11.7 MB (27.57%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -8,14 +8,15 @@ public:
         for(int i = n-1;i>=0;i--){
 
             if(digits[i] < 9){
-                digits[i]++;
+                digits[i]++;  // digit is not less than 9 so just plusone  7->8
                 return digits;
             }
 
-            digits[i]=0;
+            digits[i]=0; // digits is 9 so add 0 in that place
         }
 
-        digits.insert(digits.begin(),1);
+        digits.insert(digits.begin(),1); // all dgits are 9 so  0 will add but add 1 at start of it
+
         return digits;
     }
 };

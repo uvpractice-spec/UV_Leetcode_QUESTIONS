68deb5aeb722deceba992c8d4ad028edd14c4e9c
uvpractice-spec <uvpracticequestion@gmail.com>
2026-08-14 15:54:55 +0530

    Time: 316 ms (48.58%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -2,7 +2,8 @@
 SELECT 
     *,
     CASE 
-        WHEN (x + y > z) AND (x + z > y) AND (y + z > x) THEN 'Yes'
+        WHEN x > 0 AND y > 0 AND z > 0
+        and (x + y > z) AND (x + z > y) AND (y + z > x) THEN 'Yes'
         ELSE 'No'
     END AS triangle
 FROM Triangle;

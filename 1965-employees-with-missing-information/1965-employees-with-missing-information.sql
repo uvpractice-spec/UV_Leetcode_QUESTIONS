4771eeb5a376af82d03c55d84eab975039f3ad56
uvpractice-spec <uvpracticequestion@gmail.com>
2026-08-16 18:24:50 +0530

    Time: 598 ms (62.17%), Space: 0B (100%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+SELECT employee_id 
+FROM (
+    SELECT employee_id FROM Employees
+    UNION ALL
+    SELECT employee_id FROM Salaries
+) AS combined
+GROUP BY employee_id
+HAVING COUNT(employee_id) = 1
+ORDER BY employee_id ASC;

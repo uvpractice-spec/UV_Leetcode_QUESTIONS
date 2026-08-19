05dfd7f1dc8bbb1ad3b1f8a3868a1c5b93b26d42
uvpractice-spec <uvpracticequestion@gmail.com>
2026-08-19 23:43:21 +0530

    Time: 8 ms (81.91%), Space: 11.8 MB (81.63%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -19,7 +19,7 @@ public:
             fast = fast->next->next;    
 
             if (slow == fast) {
-                return true; // Cycle detected!
+                return true; 
             }
         }
 

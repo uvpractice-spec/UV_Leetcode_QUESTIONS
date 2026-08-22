e5e7cf0a7384af14b4bc8ea88263d5bee9adde46
uvpractice-spec <uvpracticequestion@gmail.com>
2026-08-22 23:02:23 +0530

    Time: 0 ms (100%), Space: 8 MB (99.76%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        
+        uint32_t res =0;
+
+        for(int i=0;i<32;i++){
+
+            res = (res<<1) | (n & 1);
+            n>>=1;
+        }
+
+        return res;
+    }
+};

1d88b437d6344b4dd8819fce42bbcca6f787d547
uvpractice-spec <uvpracticequestion@gmail.com>
2026-08-22 23:23:48 +0530

    Time: 0 ms (100%), Space: 10.5 MB (97.78%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        
+        vector<int> dp(n+1,0);
+
+        dp[0]=0;
+
+        for(int i=1;i<=n;i++){
+            dp[i]=dp[i>>1] + (i & 1);
+        }
+        return dp;
+    }
+};

ca5151f937266c89dc1bfeee82b2553417727e86
uvpractice-spec <uvpracticequestion@gmail.com>
2026-08-27 22:19:24 +0530

    Time: 0 ms (100%), Space: 17.8 MB (23.41%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        int maxSoFar = nums[0];
+        int currentMax = nums[0];
+        int currentMin = nums[0];
+
+        for (size_t i = 1; i < nums.size(); ++i) {
+            
+            if (nums[i] < 0) {
+                swap(currentMax, currentMin);
+            }
+
+           
+            currentMax = max(nums[i], currentMax * nums[i]);
+            currentMin = min(nums[i], currentMin * nums[i]);
+
+          
+            maxSoFar = max(maxSoFar, currentMax);
+        }
+
+        return maxSoFar;
+    }
+};

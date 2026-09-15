357e1ac181a1a2426ec3faadfcf6e1e525ca2229
uvpractice-spec <uvpracticequestion@gmail.com>
2026-09-15 21:15:43 +0530

    Time: 0 ms (100%), Space: 15.4 MB (37.45%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -9,7 +9,7 @@ public:
         int right = nums.size() - 1;
 
         while (left <= right) {
-            int mid = left + (right - left) / 2;
+           int mid = left + ((right - left) >> 1);
 
             if (nums[mid] == target) {
                 return mid;

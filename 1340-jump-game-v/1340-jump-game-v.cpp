71895d959cf4cdb91928b6068e95b9b4de87c443
uvpractice-spec <uvpracticequestion@gmail.com>
2026-09-19 19:43:25 +0530

    Time: 15 ms (43.71%), Space: 19.3 MB (32.58%) - LeetHub

--- 1340-jump-game-v/1340-jump-game-v.cpp
+++ 1340-jump-game-v/1340-jump-game-v.cpp
@@ -1,54 +1,36 @@
 class Solution {
 public:
+    int solve(int ind, int d, vector<int>& arr, vector<int>& dp){
+        if(ind<0 || ind>=arr.size()) return 0;
+
+        if(dp[ind] != -1) return dp[ind];
+        int ans=0;
+        for(int i=1;i<=d && ind+i < arr.size();i++){
+            if(arr[ind+i] >= arr[ind]) break;
+            ans=max(ans, 1+solve(ind+i,d,arr,dp));   
+        }
+
+        for(int i=1;i<=d && ind-i>=0;i++){
+            if(arr[ind-i] >= arr[ind]) break;
+            ans=max(ans, 1+solve(ind-i,d,arr,dp));
+        }
+
+        return dp[ind]=ans;
+    }
+      
     int maxJumps(vector<int>& arr, int d) {
         
        
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        int n = arr.size();
-        vector<vector<int>> adj(n);
-        vector<int> st;
+        
 
-  
-        for (int i = 0; i < n; ++i) {
-            while (!st.empty() && arr[st.back()] <= arr[i]) {
-                st.pop_back();
-            }
-            if (!st.empty() && i - st.back() <= d) {
-                adj[st.back()].push_back(i); 
-            }
-            st.push_back(i);
-        }
-
-   
-        st.clear();
-        for (int i = n - 1; i >= 0; --i) {
-            while (!st.empty() && arr[st.back()] <= arr[i]) {
-                st.pop_back();
-            }
-            if (!st.empty() && st.back() - i <= d) {
-                adj[st.back()].push_back(i); 
-            }
-            st.push_back(i);
-        }
-
-    
-        vector<int> memo(n, -1);
-        auto dfs = [&](auto& self, int u) -> int {
-            if (memo[u] != -1) return memo[u];
-            
-            int max_len = 1;
-            for (int v : adj[u]) {
-                max_len = max(max_len, 1 + self(self, v));
-            }
-            return memo[u] = max_len;
-        };
-
-        int max_jumps = 0;
-        for (int i = 0; i < n; ++i) {
-            max_jumps = max(max_jumps, dfs(dfs, i));
-        }
-
-        return max_jumps;
+      int n=arr.size();
+        vector<int> dp (n, -1);
+        int ans=0;
+        for(int i=0;i<n;i++)
+            ans=max(ans,1+solve(i,d,arr,dp));
+        return ans; 
+      
     }
 };
